2616b49f0ce3a42ac3f5b7d173153f7ec88c5af8
Кристиян Николов <stu2201261057@uni-plovdiv.bg>
2025-03-23 23:55:45 +0200

    Refactored integration test to use RestAssured

--- src/test/java/com/MythologyNexus/service/integration/MythologyIntegrationTest.java
+++ src/test/java/com/MythologyNexus/service/integration/MythologyIntegrationTest.java
@@ -1,20 +1,17 @@
 package com.MythologyNexus.service.integration;
 
 import com.MythologyNexus.model.Mythology;
-import com.MythologyNexus.repository.MythologyRepo;
-import org.junit.jupiter.api.AfterEach;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.*;
 import org.springframework.test.annotation.DirtiesContext;
 
-import static org.hamcrest.MatcherAssert.*;
-import static org.hamcrest.Matchers.greaterThanOrEqualTo;
-import static org.junit.jupiter.api.Assertions.*;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.notNullValue;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
@@ -25,22 +22,73 @@ public class MythologyIntegrationTest {
     @LocalServerPort
     private int port;
 
-    @Autowired
-    MythologyRepo mythologyRepo;
-
-    @Autowired
-    private TestRestTemplate restTemplate;
-
     private String getBaseUrl() {
         return "http://localhost:" + port + "/mythologies";
     }
 
-    @AfterEach
-    void tearDown() {
-        mythologyRepo.deleteAll();
-    }
 
     @Test
+    void testCRUDMythologyOperations() {
+        Mythology mythology = new Mythology();
+        mythology.setName("Norse Mythology");
+        mythology.setDescription("Gods and Monsters");
+
+        Integer generatedId = RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(mythology)
+                .when()
+                .post(getBaseUrl())
+            .then()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("name", equalTo("Norse Mythology"))
+                .body("description", equalTo("Gods and Monsters"))
+                .extract()
+                .path("id");
+
+        Long id = generatedId.longValue();
+        assertNotNull(id);
+
+        RestAssured.given()
+                .when()
+                .get(getBaseUrl() + "/{id}", id)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Norse Mythology"))
+                .body("description", equalTo("Gods and Monsters"));
+
+        mythology.setName("Greek Mythology");
+        RestAssured.given()
+                .contentType(ContentType.JSON)
+                .body(mythology)
+                .when()
+                .patch(getBaseUrl() + "/{id}", id)
+                .then()
+                .statusCode(200)
+                .body("name",equalTo("Greek Mythology"));
+
+        RestAssured.given()
+                .when()
+                .get(getBaseUrl() + "/{id}", id)
+                .then()
+                .statusCode(200)
+                .body("name", equalTo("Greek Mythology"));
+
+        RestAssured.given()
+                .when()
+                .delete(getBaseUrl() + "/{id}",id)
+                .then()
+                .statusCode(204);
+
+        RestAssured.given()
+                .when()
+                .get(getBaseUrl() + "/{id}", id)
+                .then()
+                .statusCode(404);
+    }
+
+
+    /*@Test
     void testMythologyCrudOperations() {
         Mythology mythology = new Mythology();
         mythology.setName("Norse Mythology");
@@ -81,5 +129,5 @@ public class MythologyIntegrationTest {
         restTemplate.delete(getBaseUrl() + "/{id}",id);
         ResponseEntity<Mythology> afterDeleteResponse = restTemplate.getForEntity(getBaseUrl() + "/{id}", Mythology.class,id);
         assertEquals(HttpStatus.NOT_FOUND,afterDeleteResponse.getStatusCode());
-    }
+    }*/
 }
