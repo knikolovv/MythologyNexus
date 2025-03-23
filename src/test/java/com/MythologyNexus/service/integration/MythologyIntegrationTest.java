7b732077afc14a374706ce5ceff3fb459beb24f6
Кристиян Николов <stu2201261057@uni-plovdiv.bg>
2025-03-23 23:56:17 +0200

    Refactored integration test to use RestAssured

--- src/test/java/com/MythologyNexus/service/integration/MythologyIntegrationTest.java
+++ src/test/java/com/MythologyNexus/service/integration/MythologyIntegrationTest.java
@@ -86,48 +86,4 @@ public class MythologyIntegrationTest {
                 .then()
                 .statusCode(404);
     }
-
-
-    /*@Test
-    void testMythologyCrudOperations() {
-        Mythology mythology = new Mythology();
-        mythology.setName("Norse Mythology");
-        mythology.setDescription("Gods and heroes from Norse region");
-
-        ResponseEntity<Mythology> createResponse = restTemplate.postForEntity(getBaseUrl(), mythology, Mythology.class);
-        assertEquals(HttpStatus.OK, createResponse.getStatusCode());
-        Mythology createMythology = createResponse.getBody();
-        assertNotNull(createMythology);
-        Long id = createMythology.getId();
-        assertNotNull(id);
-
-        ResponseEntity<Mythology> getResponse = restTemplate.getForEntity(getBaseUrl() + "/{id}", Mythology.class, id);
-        assertEquals(HttpStatus.OK, getResponse.getStatusCode());
-
-        Mythology fetchedMythology = getResponse.getBody();
-        assertNotNull(fetchedMythology);
-        assertEquals("Norse Mythology", fetchedMythology.getName());
-        assertEquals("Gods and heroes from Norse region", fetchedMythology.getDescription());
-
-        fetchedMythology.setDescription("Updated Norse Mythology Description");
-        HttpHeaders headers = new HttpHeaders();
-        headers.setContentType(MediaType.APPLICATION_JSON);
-        HttpEntity<Mythology> updateEntity = new HttpEntity<>(fetchedMythology, headers);
-        ResponseEntity<Mythology> updateResponse = restTemplate.exchange(
-                getBaseUrl() + "/{id}", HttpMethod.PATCH, updateEntity, Mythology.class, id);
-        assertEquals(HttpStatus.OK,updateResponse.getStatusCode());
-        Mythology updatedMythology = updateResponse.getBody();
-        assertNotNull(updatedMythology);
-        assertEquals("Updated Norse Mythology Description", updatedMythology.getDescription());
-
-        ResponseEntity<Mythology[]> allResponse = restTemplate.getForEntity(getBaseUrl(), Mythology[].class);
-        assertEquals(HttpStatus.OK,allResponse.getStatusCode());
-        Mythology[] mythologies = allResponse.getBody();
-        assertNotNull(mythologies);
-        assertThat(mythologies.length, greaterThanOrEqualTo(1));
-
-        restTemplate.delete(getBaseUrl() + "/{id}",id);
-        ResponseEntity<Mythology> afterDeleteResponse = restTemplate.getForEntity(getBaseUrl() + "/{id}", Mythology.class,id);
-        assertEquals(HttpStatus.NOT_FOUND,afterDeleteResponse.getStatusCode());
-    }*/
 }
